cf72180173d8474c4e1ec407c89cfe9f3850c19a
anass <anass.belcaid@gmail.com>
2026-07-31 22:04:22 +0100

    Step 32: apply_ffn_first_linear_and_relu

--- model.py
+++ model.py
@@ -264,8 +264,11 @@ def assemble_multi_head_attention_forward(
 
     return mha
 
-# Step 32 - apply_ffn_first_linear_and_relu (not yet solved)
-# TODO: implement
+# Step 32 - apply_ffn_first_linear_and_relu
+def apply_ffn_first_linear_and_relu(x, w1, b1):
+    # TODO: project x by w1, add b1, then apply a ReLU activation.
+    projection = x @ w1 + b1
+    return torch.relu(projection)
 
 # Step 33 - apply_ffn_second_linear (not yet solved)
 # TODO: implement
