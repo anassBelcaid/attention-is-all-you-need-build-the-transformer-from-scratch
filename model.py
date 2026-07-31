2c648e0c9c0af3b4c1cb10a2ef4fff1db0e62827
anass <anass.belcaid@gmail.com>
2026-07-31 13:49:45 +0100

    Step 2: build_id_to_token_vocab

--- model.py
+++ model.py
@@ -17,8 +17,10 @@ def build_token_to_id_vocab(sentences, specials=("<pad>", "<bos>", "<eos>", "<un
                 cache[word] = len(cache)
     return cache
 
-# Step 2 - build_id_to_token_vocab (not yet solved)
-# TODO: implement
+# Step 2 - build_id_to_token_vocab
+def build_id_to_token_vocab(token_to_id):
+    # TODO: build the inverse id-to-token dictionary from token_to_id
+    return {i: token for token, i in token_to_id.items()}
 
 # Step 3 - encode_sentence_to_ids (not yet solved)
 # TODO: implement
